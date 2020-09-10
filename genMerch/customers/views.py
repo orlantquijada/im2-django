dc2391f8f58fb2599994f6d12dc699c320b70974
Orlan Quijada <orlantquijada@gmail.com>
2020-09-11 04:08:14 +0800

    product reg

--- genMerch/customers/views.py
+++ genMerch/customers/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 def customer_index_view(request):
     return render(request, 'customers/customers.html', {'is_customer': True})
+
 
 def customer_registration_view(request):
     return render(request, 'customers/customer_reg.html', {'is_customer': True})
