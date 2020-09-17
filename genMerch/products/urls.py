9420ee774b98abf5c23948b88779dfdf50f9afa2
Orlan Quijada <orlantquijada@gmail.com>
2020-09-17 21:33:34 +0800

    updated products views to use custom template view

--- genMerch/products/urls.py
+++ genMerch/products/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('index', views.product_index_view, name="dashboard"),
-   # path('registration', views.product_registration_view, name="registration")
-    path('registration', views.ProductRegistrationView.as_view(), name="registration")
+    path('index', views.ProductIndexTemplateView.as_view(), name="dashboard"),
+    path('registration', views.ProductRegistrationTemplateView.as_view(), name="registration")
 ]
--- genMerch/products/views.py
+++ genMerch/products/views.py
@@ -1,51 +1,41 @@
-from django.shortcuts import render
-from .forms import ProductForm,ProductImageForm
-from .models import *
-from django.views.generic import View
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.db import transaction
 
-def product_index_view(request):
-    return render(request, 'products/products.html', {'is_customer': False})
+from products import models, forms
+from genMerch import views as custom_views
 
-def product_registration_view(request):
-    return render(request, 'products/product_reg.html', {'is_customer': False})
 
-class ProductRegistrationView(View):
-    def get(self, request):
-        return render (request, 'products/product_reg.html')
-    
+class ProductIndexTemplateView(custom_views.CustomTemplateView):
+    template_name = 'products/products.html'
+    queryset = models.Product.objects.all()  # pylint: disable=no-member
+
+
+class ProductRegistrationTemplateView(custom_views.CustomTemplateView):
+    template_name = 'products/product_reg.html'
+    queryset = models.Product.objects.all()  # pylint: disable=no-member
+    default_form = forms.ProductForm
+
+    @transaction.atomic
     def post(self, request):
-        form = ProductForm(request.POST,request.FILES)
-        form2 = ProductImageForm(request.POST,request.FILES)
-       # prodname = request.FILES["img1"]
-       # print(prodname)
-        if form.is_valid():
-            date_reg = request.POST.get("date_reg")
-            prodName = request.POST.get("prod_name")
-            category = request.POST.get("category")
-            brand = request.POST.get("prodbrand")
-            size = request.POST.get("prodSize")
-            color = request.POST.get("prodColor")
-            price = request.POST.get("prodPrice")
-            stock = request.POST.get("prodStocks")
-            img1 = request.FILES["img1"]
-            img2 = request.FILES["img2"]
-            img3= request.FILES["img3"]
+        form = self.default_form(request.POST, request.FILES)
 
-            form = Product(date_registered = date_reg, prod_name = prodName, category = category, brand = brand,
-                                color = color, size = size, price = price, stocks = stock)
+        if form.is_valid():
             form.save()
 
-            form2 = ProductImage(product = form,image = img1)
-            form2.save()
-            form2 = ProductImage(product = form,image = img2)
-            form2.save()
-            form2 = ProductImage(product = form,image = img3)
-            form2.save()
+            img1 = form.cleaned_data.get('img1')
+            img2 = form.cleaned_data.get('img2')
+            img3 = form.cleaned_data.get('img3')
 
-            return HttpResponse('Product Record Saved!')
+            instance = form.instance
+            for img in (img1, img2, img3):
+                # images are not required
+                if not img:
+                    continue
 
-        else:
-            print(form.errors)
-            return HttpResponse('not valid')
+                product_image = models.ProductImage(
+                    product=instance, image=img)
+                product_image.save()
+                instance.product_images.add(product_image)
 
+            return redirect(reverse('products:dashboard'))
+        return render(request, 'products/product_reg.html')
