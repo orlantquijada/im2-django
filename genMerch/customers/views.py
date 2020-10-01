3dce1421ebc29c234b008aca61a1e167e3a74e22
Orlan Quijada <orlantquijada@gmail.com>
2020-10-02 02:35:42 +0800

    update delete customers

--- genMerch/customers/views.py
+++ genMerch/customers/views.py
@@ -5,58 +5,36 @@ from genMerch import views as custom_views
 
 
 class CustomerIndexTemplateView(custom_views.CustomTemplateView):
-    template_name = 'customers/customers.html'
+    template_name = "customers/customers.html"
     queryset = models.Customer.objects.all()  # pylint: disable=no-member
     default_form = forms.CustomerRegistrationModelForm
-    default_context = {'is_customer': True}
+    default_context = {"is_customer": True}
 
-    def post(self,request):
-        if request.method == 'POST':
-            if 'btnUpdate' in request.POST:
-                print("update clicked")
-                cid = request.POST.get("cid")
-                fname = request.POST.get("first_name")
-                mname = request.POST.get("middle_name")
-                lname = request.POST.get("last_name")
-                email = request.POST.get("email")
-                pnumber = request.POST.get("phone_number")
-                city = request.POST.get("city")
-                prov = request.POST.get("province")
-                zipc = request.POST.get("zip_code")
-                conty = request.POST.get("country")
-                bdate = request.POST.get("birthdate")
-                gender = request.POST.get("gender")
-                stat = request.POST.get("status")
-                spouse = request.POST.get("spouse_name")
-                spouseOcc = request.POST.get("spouse_occupation")
-                children = request.POST.get("number_of_children")
-                mother =request.POST.get("mother_name")
-                motherOcc = request.POST.get("mother_occupation")
-                father = request.POST.get("father_name")
-                fatherOCc = request.POST.get("father_occupation")
-                height = request.POST.get("height")
-                weight = request.POST.get("weight")
-                religion = request.POST.get("religion")
-                pp = request.FILES["profile_pic"]
+    def post(self, request):
+        customer_id = int(request.POST.get("id"))
+        # pylint: disable=no-member
+        customer_instance = models.Customer.objects.get(id=customer_id)
 
-                models.Customer.objects.filter(person_ptr_id = cid).update(first_name = fname, middle_name = mname, last_name = lname, profile_pic = pp,   # pylint: disable=no-member
-                                city= city, province = prov, zip_code = zipc, country = conty, sex = gender, birthdate=bdate, status=stat,
-                                spouse_name = spouse,spouse_occupation = spouseOcc, number_of_children = children, father_name = father,
-                                father_occupation = fatherOCc,mother_name = mother, mother_occupation = motherOcc, height=height,weight=weight,
-                                religion = religion, email = email, phone_number = pnumber )
-                print("customer updated")    
-            elif 'btnDelete' in request.POST:
-                cid = request.POST.get("cid")
-                models.Customer.objects.filter(person_ptr_id = cid).delete() # pylint: disable=no-member
-                models.Person.objects.filter(id = cid).delete() # pylint: disable=no-member
-                print("customer deleted")
-        return redirect('customers:dashboard')
+        if "delete" in request.POST:
+            customer_instance.delete()
+
+            return redirect("customers:dashboard")
+
+        form = self.default_form(
+            request.POST, request.FILES, instance=customer_instance
+        )
+
+        if form.is_valid():
+            form.save()
+
+        return redirect("customers:dashboard")
+
 
 class CustomerRegistrationTemplateView(custom_views.CustomTemplateView):
-    template_name = 'customers/customer_reg.html'
+    template_name = "customers/customer_reg.html"
     queryset = models.Customer.objects.all()  # pylint: disable=no-member
     default_form = forms.CustomerRegistrationModelForm
-    default_context = {'is_customer': True}
+    default_context = {"is_customer": True}
 
     def post(self, request):
         form = self.default_form(request.POST, request.FILES)
@@ -64,9 +42,9 @@ class CustomerRegistrationTemplateView(custom_views.CustomTemplateView):
         if form.is_valid():
             form.save()
 
-            return redirect(reverse('customers:dashboard'))
+            return redirect(reverse("customers:dashboard"))
 
         context = self.get_context_data()
-        context['has_error'] = True
+        context["has_error"] = True
 
         return render(request, self.template_name, context=context)
