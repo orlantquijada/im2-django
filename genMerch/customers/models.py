95a2c0dffb71497fb8b2bdd4be801a8ca927531e
Orlan Quijada <orlantquijada@gmail.com>
2020-10-15 03:59:30 +0800

    added soft delete to customers models

--- genMerch/customers/models.py
+++ genMerch/customers/models.py
@@ -5,72 +5,73 @@ from django.core.validators import MinLengthValidator
 
 from genMerch import globals, fields as custom_fields
 from customers import choices
+from main import models as main_models
 
 
-class Person(models.Model):
-    first_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH)
+class Person(main_models.SoftDeletionModel):
+    first_name = custom_fields.TitleCaseCharfield(max_length=globals.DEFAULT_MAX_LENGTH)
     middle_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default='')
-    last_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH)
+        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default=""
+    )
+    last_name = custom_fields.TitleCaseCharfield(max_length=globals.DEFAULT_MAX_LENGTH)
 
     email = models.EmailField(null=True, blank=True, unique=True)
     phone_number = custom_fields.PhoneNumberField(null=True, blank=True)
 
-    profile_pic = models.ImageField(
-        upload_to='images/customers/profile-pics/')
+    profile_pic = models.ImageField(upload_to="images/customers/profile-pics/")
 
-    city = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH)
-    province = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH)
+    city = custom_fields.TitleCaseCharfield(max_length=globals.DEFAULT_MAX_LENGTH)
+    province = custom_fields.TitleCaseCharfield(max_length=globals.DEFAULT_MAX_LENGTH)
     zip_code = models.PositiveIntegerField()
-    country = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH)
+    country = custom_fields.TitleCaseCharfield(max_length=globals.DEFAULT_MAX_LENGTH)
 
     sex = models.CharField(
-        max_length=1, choices=choices.Sex.choices, default=choices.Sex.MALE)
+        max_length=1, choices=choices.Sex.choices, default=choices.Sex.MALE
+    )
     birthdate = models.DateField()
     status = models.CharField(
-        max_length=1, choices=choices.Status.choices, default=choices.Status.SINGLE)
+        max_length=1, choices=choices.Status.choices, default=choices.Status.SINGLE
+    )
 
     spouse_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.LONG_MAX_LENGTH, null=True, blank=True, default='')
+        max_length=globals.LONG_MAX_LENGTH, null=True, blank=True, default=""
+    )
     spouse_occupation = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default='')
+        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default=""
+    )
     number_of_children = models.PositiveIntegerField(default=0)
 
-    father_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.LONG_MAX_LENGTH)
+    father_name = custom_fields.TitleCaseCharfield(max_length=globals.LONG_MAX_LENGTH)
     father_occupation = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default='')
+        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default=""
+    )
 
-    mother_name = custom_fields.TitleCaseCharfield(
-        max_length=globals.LONG_MAX_LENGTH)
+    mother_name = custom_fields.TitleCaseCharfield(max_length=globals.LONG_MAX_LENGTH)
     mother_occupation = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default='')
+        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default=""
+    )
 
-    height = models.FloatField(verbose_name='Height (cm)')
-    weight = models.FloatField(verbose_name='weight (kg)')
+    height = models.FloatField(verbose_name="Height (cm)")
+    weight = models.FloatField(verbose_name="weight (kg)")
     religion = custom_fields.TitleCaseCharfield(
-        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default='')
+        max_length=globals.DEFAULT_MAX_LENGTH, null=True, blank=True, default=""
+    )
 
     def __str__(self):
         # pylint: disable=no-member
-        return f'{self.id} / { self.full_name }'
+        return f"{self.id} / { self.full_name }"
 
     @property
     def full_name(self):
         middle_name = self.middle_name
         if middle_name:
             # pylint: disable=unsubscriptable-object
-            return f'{self.first_name} {middle_name[0]}. {self.last_name}'
-        return f'{self.first_name} {self.last_name}'
+            return f"{self.first_name} {middle_name[0]}. {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
     @property
     def address(self):
-        return f'{self.city}, {self.province} {self.country} {self.zip_code}'
+        return f"{self.city}, {self.province} {self.country} {self.zip_code}"
 
     class Meta:
         db_table = "Person"
